0ff866712326c7e5aabfd95605daff363f7d2536
Remco Schellekens <remco.schellekens@ns.nl>
2018-03-30 11:47:13 +0200

    small update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-rm -f rsrc.syso owig.exe
+rm -f rsrc.syso bin/owig.exe 
 rsrc -manifest owig.manifest -ico "owig256.ico,owig48.ico,owig32.ico,owig16.ico" -o rsrc.syso
-go build -ldflags="-H windowsgui"
+go build -ldflags="-H windowsgui" -o bin/owig.exe
